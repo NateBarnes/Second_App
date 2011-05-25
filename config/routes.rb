df95f4e9a9c982882f3c0d8d3cc172c74a60e7e6
Nathaniel Barnes <kerianambrai@gmail.com>
2011-05-24 23:00:40 -0500

    Added follow and unfollow functionality and the beginnings of the views for them

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 SecondApp::Application.routes.draw do
   resources :users do
+    member do
+      get :following, :followers
+    end
     resources :microposts, :only => :index
   end
   resources :sessions, :only => [:new, :create, :destroy]
   resources :microposts, :only => [:create, :destroy]
+  resources :relationships, :only => [:create, :destroy]
 
   root :to => "pages#home"
   
